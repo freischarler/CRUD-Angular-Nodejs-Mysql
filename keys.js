d6cd05dc7fd5960a3b16e4a8699f9cbb7b9f2d5f
freischarler <martin.paz@live.com.ar>
2020-07-31 14:09:18 -0300

    nodejs-mysql

---
+++ keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'ng_canna_db'
+        //port: 3000,
+    }
+};
---
+++ routes/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'asd',
+        database: 'ng_canna_db'
+        //port: 3000,
+    }
+};
