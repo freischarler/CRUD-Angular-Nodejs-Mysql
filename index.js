b1cc00828c17bbf050a4c8579ee049819e61e1af
freischarler <martin.paz@live.com.ar>
2020-08-01 11:35:15 -0300

    node+angular3.1

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const valuesRoutes_1 = __importDefault(require("./routes/valuesRoutes"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
@@ -21,10 +20,11 @@ class Server {
         this.app.use(cors_1.default());
         this.app.use(express_1.default.json()); //el servidor entiende el formato json
         //this.app.use(express.static(__dirname + '/dist'));
-        this.app.use(express_1.default.static(path.join(__dirname, '../dist/')));
+        //this.app.use(express.static(path.join(__dirname,'../dist/')));
     }
     routes() {
-        this.app.use('/', indexRoutes_1.default);
+        //this.app.use('/', indexRoutes);
+        this.app.use('/', express_1.default.static('../dist'));
         this.app.use('/api/values', valuesRoutes_1.default);
     }
     start() {
