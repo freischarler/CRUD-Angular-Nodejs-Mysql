94728b70b194d3f85122fef9e62dcd412bfd93b1
freischarler <martin.paz@live.com.ar>
2020-08-01 11:19:24 -0300

    node+angular2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const valuesRoutes_1 = __importDefault(require("./routes/valuesRoutes"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+var path = require('path');
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -19,7 +20,8 @@ class Server {
         this.app.use(morgan_1.default('dev')); //para ver los GET POST en el npm
         this.app.use(cors_1.default());
         this.app.use(express_1.default.json()); //el servidor entiende el formato json
-        this.app.use(express_1.default.static(__dirname + '/dist'));
+        //this.app.use(express.static(__dirname + '/dist'));
+        this.app.use(express_1.default.static(path.join(__dirname, 'dist/')));
     }
     routes() {
         this.app.use('/', indexRoutes_1.default);
